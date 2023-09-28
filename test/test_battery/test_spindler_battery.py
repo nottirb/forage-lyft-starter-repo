fc16e01d64d69437eb4dda99b3b5236719fb5676
nottrib <brittonkfischer@gmail.com>
2023-09-27 22:47:33 -0700

    feat: Upgrade Spindler battery

--- test/test_battery/test_spindler_battery.py
+++ test/test_battery/test_spindler_battery.py
@@ -7,12 +7,12 @@ from batteries.spindler_battery import SpindlerBattery
 class TestSpindlerBattery(TestCase):
     def test_should_need_service(self):
         today = datetime.today().date()
-        last_service_date = today.replace(year = today.year - 3)
+        last_service_date = today.replace(year = today.year - 4)
         battery = SpindlerBattery(today, last_service_date)
         self.assertTrue(battery.needs_service())
         
     def test_should_not_need_service(self):
         today = datetime.today().date()
-        last_service_date = today.replace(year = today.year - 1)
+        last_service_date = today.replace(year = today.year - 3)
         battery = SpindlerBattery(today, last_service_date)
         self.assertFalse(battery.needs_service())
